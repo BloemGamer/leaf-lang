350b5c1c2773e11dceee6584f4fe6b2b1d2e5788
Bloem <sem@ie-design.nl>
2025-11-04 11:01:34 +0100

    added some tokens

--- include/lexer.h
+++ include/lexer.h
@@ -29,13 +29,13 @@
 	X(token_type_arrow)				/* -> */ \
 	X(token_type_lshift)			/* << */ \
 	X(token_type_rshift)			/* >> */ \
-\
+	X(token_type_and)				/* && */ \
+	X(token_type_or)				/* || */ \
 	/*ompound tokens: */ \
 	X(token_type_bang_equal)		/* != */ \
 	X(token_type_equal_equal)		/* == */ \
 	X(token_type_greater_equal)		/* >= */ \
 	X(token_type_less_equal)		/* <= */ \
-\
 	/*ignored:*/ \
 	X(token_type_whitespace)		/* \t, \n, \r or <space> */ \
 	X(token_type_comment)			/* // (both comments) */ \
@@ -48,18 +48,16 @@
 \
 	/*identifier: */ \
 	X(token_type_identifier)		/* id */ \
+	X(token_type_type)				/* type */ \
 \
 	/*keywords: */ \
 	X(token_type_if)				/* if */ \
 	X(token_type_else)				/* else */ \
-	X(token_type_and)				/* && */ \
-	X(token_type_or)				/* || */ \
-	X(token_type_false)				/* false */ \
 	X(token_type_true)				/* true */ \
-	X(token_type_while)				/* while loop */ \
-	X(token_type_for)				/* for loop */ \
-	X(token_type_var)				/* variable */ \
-	X(token_type_fun)				/* function */ \
+	X(token_type_false)				/* false */ \
+	X(token_type_while)				/* while */ \
+	X(token_type_for)				/* for */ \
+	X(token_type_fun)				/* fn */ \
 	X(token_type_return)			/* return */
 
 
@@ -68,64 +66,6 @@ typedef enum
 #define X(x) x,
 	__tokens
 #undef X
-	// token_type_eof,				// always appended as the End Of File (Last token, much like string terminator)
-	// token_type_invalid,			// only used for errors
-	//
-	// //simple tokens:
-	// token_type_lparen,			// (
-	// token_type_rparen,			// )
-	// token_type_lbrace,			// {
-	// token_type_rbrace,			// }
-	// token_type_lsqbracket,		// [
-	// token_type_rsqbracket,		// ]
-	// token_type_dot,				// .
-	// token_type_comma,			// ,
-	// token_type_plus,			// +
-	// token_type_minus,			// -
-	// token_type_star,			// *
-	// token_type_slash,			// /
-	// token_type_semicolon,		// ;
-	// token_type_bang,			// !
-	// token_type_equal,			// =
-	// token_type_greater,			// >
-	// token_type_less,			// <
-	// token_type_ampersand,		// &
-	// token_type_caret,			// ^
-	// token_type_pipe,			// |
-	// token_type_tilda,			// ~
-	//
-	// //compound tokens:
-	// token_type_bang_equal,		// !=
-	// token_type_equal_equal,		// ==
-	// token_type_greater_equal,	// >=
-	// token_type_less_equal,		// <=
-	//
-	// //ignored:
-	// token_type_whitespace,		// \t, \n, \r or <space>
-	// token_type_comment,			// //
-	//
-	// //literals:
-	// token_type_string,			// string literal "..."
-	// token_type_invalid_string,	// invalid string literal "... EOF
-	// token_type_number,			// any number like: 5 or 5.23
-	// token_type_char,			// any char like : 'E'
-	//
-	// //identifier:
-	// token_type_identifier,		// id
-	//
-	// //keywords:
-	// token_type_if,				// if
-	// token_type_else,			// else
-	// token_type_and,				// &&
-	// token_type_or,				// ||
-	// token_type_false,			// false
-	// token_type_true,			// true
-	// token_type_while,			// while loop
-	// token_type_for,				// for loop
-	// token_type_var,				// variable
-	// token_type_fun,				// function
-	// token_type_return,			// return
-
 } Token_type;
 
 typedef struct
