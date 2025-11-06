3c2036fc95e93c16dab5664c6aa97ce40e1bdb6c
Bloem <sem@ie-design.nl>
2025-11-06 15:03:06 +0100

    added some typedefs for nicer writing

--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <stdint.h>
+
 #define MAX(a, b) ((a) > (b)) ? (a) : (b)
 #define MIN(a, b) ((a) < (b)) ? (a) : (b)
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
