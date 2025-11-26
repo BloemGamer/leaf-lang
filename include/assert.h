5dc1701e19b7cb9074d17760e2c8956aece3a1bf
Bloem <sem@ie-design.nl>
2025-11-26 23:56:03 +0100

    updated the assert macro's

--- include/assert.h
+++ include/assert.h
@@ -1,41 +1,28 @@
 #pragma once
 
-// First include with NDEBUG undefined to get the "always on" version
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+
+noreturn static inline void assert_abort(void)
+{
+	abort();
+}
+
+#define assert(expr, ...)                                                                \
+	{                                                                                    \
+		if (!(expr))                                                                     \
+		{                                                                                \
+			(void)fprintf(stderr, "Assertion failed: %s\n", #expr);                      \
+			/* print extra message only if __VA_ARGS__ exists */                         \
+			__VA_OPT__((void)fprintf(stderr, __VA_ARGS__); (void)fprintf(stderr, "\n");) \
+			(void)fprintf(stderr, "File: %s\nLine: %d\n", __FILE__, __LINE__);           \
+			assert_abort();                                                              \
+		}                                                                                \
+	}
+
 #ifdef NDEBUG
-    #define NDEBUG_WAS_DEFINED
-    #undef NDEBUG
-#endif
-
-#include_next <assert.h>
-
-// Immediately save the real assert implementation before doing anything else
-#ifdef __assert_fail
-    #define __assert_fail_always __assert_fail
-#else
-    // Capture the macro expansion of assert itself
-    #define assert_always_impl(expr) assert(expr)
-#endif
-
-#ifdef NDEBUG_WAS_DEFINED
-    #define NDEBUG
-    #undef NDEBUG_WAS_DEFINED
-#endif
-
-// Undefine assert
-#undef assert
-
-// Define debug_assert based on NDEBUG
-#ifdef NDEBUG
-    #define debug_assert(expr) ((void)0)
-#else
-    #define debug_assert(expr) assert_always_impl(expr)
-#endif
-
-// Redefine assert to always be active (re-include with NDEBUG off)
-#ifdef NDEBUG
-    #undef NDEBUG
-    #include_next <assert.h>
-    #define NDEBUG
-#else
-    #include_next <assert.h>
+#define debug_assert(expr, ...) (void)0
+#else /* NDEBUG */
+#define debug_assert(expr, ...) assert(expr, __VA_ARGS__)
 #endif
