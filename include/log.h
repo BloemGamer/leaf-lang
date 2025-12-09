0161ec057c9048fb941e034b6a79b315d4c76319
Bloem <sem@ie-design.nl>
2025-12-09 23:23:07 +0100

    updated the LOG to now use errprintf

--- include/log.h
+++ include/log.h
@@ -4,10 +4,9 @@
 
 #define LOG_WARN(pos, format, ...) LOG("\x1B[33m", "WARNING", pos, format __VA_OPT__(, ) __VA_ARGS__)
 
-#define LOG(colour_str, log_type_str, pos, format, ...)          \
-	(void)fprintf(stderr,                                        \
-				  colour_str "[line %zu:%zu] " log_type_str ": " \
-							 "\x1B[0m" format,                   \
-				  pos.line, pos.character __VA_OPT__(, ) __VA_ARGS__)
+#define LOG(colour_str, log_type_str, pos, format, ...)            \
+	(void)errprintf(colour_str "[line %zu:%zu] " log_type_str ": " \
+							   "\x1B[0m" format,                   \
+					pos.line, pos.character __VA_OPT__(, ) __VA_ARGS__)
 
 #define errprintf(...) fprintf(stderr, __VA_ARGS__)
