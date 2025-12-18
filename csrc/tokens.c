af6f6941433870262e475b67e3dce17c475add2d
Bloem <sem@ie-design.nl>
2025-12-18 20:31:21 +0100

    removed everything from C

--- csrc/tokens.c
+++
@@ -1,3 +0,0 @@
-#include "tokens.h"
-
-const char* token_to_string(const TokenType token) { return TOKENS_STR_PR[token]; }
--- include/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <inttypes.h>
-#include <stddef.h>
-#include <stdint.h>
-
-#define MAX(a, b) ((a) > (b)) ? (a) : (b)
-#define MIN(a, b) ((a) < (b)) ? (a) : (b)
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
