1f14c816caf2bec874040d109737142d8b158d71
Bloem <sem@ie-design.nl>
2025-12-19 15:55:21 +0100

    added a file input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::process::exit;
+
 mod lexer;
 
 #[derive(Debug, PartialEq, Clone, Default)]
@@ -6,6 +9,12 @@ pub struct Config {}
 fn main()
 {
 	let config: Config = Config::default();
-	let lexed = lexer::Lexer::new(&config, "fn main{i64 a = 0; return a}");
+	let file: String = if let Ok(f) = fs::read_to_string("slang-test/main.sl") {
+		f
+	} else {
+		println!("could not open file");
+		exit(1)
+	};
+	let lexed = lexer::Lexer::new(&config, &file);
 	println!("{:#?}", lexed.into_iter().collect::<Vec<_>>());
 }
